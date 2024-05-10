80731b3236c48083205112fc6d98bc7f38b8c968
Oscar Devs <ohernandezba@gmail.com>
2024-05-10 13:45:08 +0200

    Salto Player y GroundCheck

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -10,6 +10,16 @@ public class PlayerControls : MonoBehaviour
     private Rigidbody2D rb;
     private bool facingRight = true;
 
+    [Header("Jump")]
+    [SerializeField] private float jumpForce;
+
+    [Header("Ground Check")]
+    [SerializeField] private float rayLength;
+    [SerializeField] private Transform leftPoint;
+    [SerializeField] private Transform rightPoint;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,7 +32,9 @@ public class PlayerControls : MonoBehaviour
 
     void FixedUpdate()
     {
+        GroundCheck();
         Move();
+        Jump();
     }
 
     private void Move()
@@ -42,5 +54,23 @@ public class PlayerControls : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
             facingRight = !facingRight;
         }
+    }
+
+    private void Jump()
+    {
+        if ((gInput.tryToJump))
+        {
+            if (isGrounded)
+                rb.velocity = new Vector2(speed * gInput.valueX, jumpForce);
+            gInput.tryToJump = false;
+        }
+    }
+
+    private void GroundCheck()
+    {
+        RaycastHit2D hitLeft = Physics2D.Raycast(leftPoint.position, Vector2.down, rayLength, groundLayer);
+        RaycastHit2D hitRight = Physics2D.Raycast(rightPoint.position, Vector2.down, rayLength, groundLayer);
+
+        isGrounded = hitLeft || hitRight;
     }
 }
