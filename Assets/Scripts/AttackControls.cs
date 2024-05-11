df8fa91acdf31e3e26f11b75f7c309be5f4d70e2
Oscar Devs <ohernandezba@gmail.com>
2024-05-12 00:09:14 +0200

    Animación de ataque

--- Assets/Scripts/AttackControls.cs
+++ Assets/Scripts/AttackControls.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class AttackControls : MonoBehaviour
 {
     [SerializeField] private GatherInput gInput;
+    private Animator animator;
+    public bool attackStarted;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         if (gInput.tryToAttack)
         {
+            if (!attackStarted)
+            {
+                attackStarted = true;
+                animator.SetBool("Attack", attackStarted);
+            }
             gInput.tryToAttack = false;
         }
+    }
+
+    public void Reset()
+    {
+        attackStarted = false;
+        // disable the attack collider
+
+        // call this if you die
+        animator.SetBool("Attack", attackStarted);
     }
 }
